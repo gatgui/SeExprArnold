6e77e7aa04d35a534a6927c4597361db61d8a994
Gaetan Guidet <guidet@marza.com>
2016-10-07 18:07:27 +0900

    fix issues with connected variables in maya AE

--- maya/aiSeexprTemplate.py
+++ maya/aiSeexprTemplate.py
@@ -59,8 +59,19 @@ class AEaiSeexprTemplate(ShaderAETemplate):
    def updateFloatVariableValue(self, attr, fld):
       cmds.setAttr(attr, cmds.floatField(fld, query=1, value=1))
    
+   def updateFloatVariableConnectivity(self, attr, fld):
+      conn = (cmds.listConnections(attr, s=1, d=0) is not None)
+      # (0.871, 0.447, 0.478) => animation
+      # (0.945, 0.945, 0.647) => other connections
+      # (0.165, 0.165, 0.165) => editable
+      if conn:
+         cmds.floatField(fld, edit=1, editable=False, backgroundColor=(0.945, 0.945, 0.647))
+      else:
+         cmds.floatField(fld, edit=1, editable=True, backgroundColor=(0.165, 0.165, 0.165))
+   
    def setupFloatVariableValueCallback(self, attr, fld):
       cmds.floatField(fld, edit=1, changeCommand=lambda *args: self.updateFloatVariableValue(attr, fld))
+      cmds.scriptJob(killWithScene=1, parent=fld, replacePrevious=True, connectionChange=(attr, lambda *args: self.updateFloatVariableConnectivity(attr, fld)))
    
    def removeFloatVariable(self, nodeAttr, varslayout, index):
       # Remove variable
@@ -71,6 +82,8 @@ class AEaiSeexprTemplate(ShaderAETemplate):
       
       baseNameAttr = nodeAttr
       baseValueAttr = nodeAttr.replace("fparam_name", "fparam_value");
+      if baseValueAttr == nodeAttr:
+         baseValueAttr = nodeAttr.replace("fparamName", "fparamValue")
       
       for i in xrange(index+1, len(children)):
          rembtn, namefld, _, valfld = cmds.formLayout(children[i], query=1, childArray=1)
@@ -81,11 +94,28 @@ class AEaiSeexprTemplate(ShaderAETemplate):
          nameAttr = baseNameAttr + indexStr
          valueAttr = baseValueAttr + indexStr
          
-         cmds.setAttr(nameAttr, cmds.getAttr(baseNameAttr + nextIndexStr), type="string")
-         cmds.setAttr(valueAttr, cmds.getAttr(baseValueAttr + nextIndexStr));
+         conns = cmds.listConnections(nameAttr, s=1, d=0, sh=1, p=1)
+         if conns:
+            cmds.disconnectAttr(conns[0], nameAttr)
+         conns = cmds.listConnections(baseNameAttr + nextIndexStr, s=1, d=0, sh=1, p=1)
+         if conns:
+            cmds.connectAttr(conns[0], nameAttr)
+         else:
+            cmds.setAttr(nameAttr, cmds.getAttr(baseNameAttr + nextIndexStr), type="string")
+         
+         conns = cmds.listConnections(valueAttr, s=1, d=0, sh=1, p=1)
+         if conns:
+            cmds.disconnectAttr(conns[0], valueAttr)
+         conns = cmds.listConnections(baseValueAttr + nextIndexStr, s=1, d=0, sh=1, p=1)
+         if conns:
+            cmds.connectAttr(conns[0], valueAttr)
+         else:
+            cmds.setAttr(valueAttr, cmds.getAttr(baseValueAttr + nextIndexStr))
          
          self.setupVariableNameCallback(nameAttr, namefld)
          self.setupFloatVariableValueCallback(valueAttr, valfld)
+         self.updateFloatVariableConnectivity(valueAttr, valfld)
+         
          cmds.button(rembtn, edit=1, command=lambda *args: self.removeFloatVariable(nodeAttr, varslayout, i-1))
       
       cmds.deleteUI(children[index])
@@ -107,11 +137,14 @@ class AEaiSeexprTemplate(ShaderAETemplate):
       vallbl = cmds.text(label="=")
       valfld = cmds.floatField(value=(0.0 if value is None else value))
       
-      cmds.setAttr(nameAttr, ("fparam%d" % n if name is None else name), type="string")
-      cmds.setAttr(valueAttr, (0.0 if value is None else value))
+      if not cmds.listConnections(nameAttr, s=1, d=0):
+         cmds.setAttr(nameAttr, ("fparam%d" % n if name is None else name), type="string")
+      if not cmds.listConnections(valueAttr, s=1, d=0):
+         cmds.setAttr(valueAttr, (0.0 if value is None else value))
       
       self.setupVariableNameCallback(nameAttr, namefld)
       self.setupFloatVariableValueCallback(valueAttr, valfld)
+      self.updateFloatVariableConnectivity(valueAttr, valfld)
       
       cmds.button(rembtn, edit=1, command=lambda *args: self.removeFloatVariable(nodeAttr, varslayout, n))
       
@@ -132,12 +165,40 @@ class AEaiSeexprTemplate(ShaderAETemplate):
       x = cmds.floatField(fld0, query=1, value=1)
       y = cmds.floatField(fld1, query=1, value=1)
       z = cmds.floatField(fld2, query=1, value=1)
-      cmds.setAttr(attr, x, y, z);
+      #cmds.setAttr(attr, x, y, z);
+      if cmds.listConnections(attr+".vparamValueX", s=1, d=0) is None:
+         cmds.setAttr(attr+".vparamValueX", x)
+      if cmds.listConnections(attr+".vparamValueY", s=1, d=0) is None:
+         cmds.setAttr(attr+".vparamValueY", y)
+      if cmds.listConnections(attr+".vparamValueZ", s=1, d=0) is None:
+         cmds.setAttr(attr+".vparamValueZ", z)
+   
+   def updateVectorVariableConnectivity(self, attr, fld0, fld1, fld2):
+      xconn = (cmds.listConnections(attr+".vparamValueX", s=1, d=0) is not None)
+      yconn = (cmds.listConnections(attr+".vparamValueY", s=1, d=0) is not None)
+      zconn = (cmds.listConnections(attr+".vparamValueZ", s=1, d=0) is not None)
+      fullconn = (cmds.listConnections(attr, s=1, d=0) is not None)
+      # (0.871, 0.447, 0.478) => animation
+      # (0.945, 0.945, 0.647) => other connections
+      # (0.165, 0.165, 0.165) => editable
+      if xconn or fullconn:
+         cmds.floatField(fld0, edit=1, editable=False, backgroundColor=(0.945, 0.945, 0.647))
+      else:
+         cmds.floatField(fld0, edit=1, editable=True, backgroundColor=(0.165, 0.165, 0.165))
+      if yconn or fullconn:
+         cmds.floatField(fld1, edit=1, editable=False, backgroundColor=(0.945, 0.945, 0.647))
+      else:
+         cmds.floatField(fld1, edit=1, editable=True, backgroundColor=(0.165, 0.165, 0.165))
+      if zconn or fullconn:
+         cmds.floatField(fld2, edit=1, editable=False, backgroundColor=(0.945, 0.945, 0.647))
+      else:
+         cmds.floatField(fld2, edit=1, editable=True, backgroundColor=(0.165, 0.165, 0.165))
    
    def setupVectorVariableValueCallback(self, attr, fld0, fld1, fld2):
       cmds.floatField(fld0, edit=1, changeCommand=lambda *args: self.updateVectorVariableValue(attr, fld0, fld1, fld2))
       cmds.floatField(fld1, edit=1, changeCommand=lambda *args: self.updateVectorVariableValue(attr, fld0, fld1, fld2))
       cmds.floatField(fld2, edit=1, changeCommand=lambda *args: self.updateVectorVariableValue(attr, fld0, fld1, fld2))
+      cmds.scriptJob(killWithScene=1, parent=fld0, replacePrevious=True, connectionChange=(attr, lambda *args: self.updateVectorVariableConnectivity(attr, fld0, fld1, fld2)))
    
    def removeVectorVariable(self, nodeAttr, varslayout, index):
       children = cmds.columnLayout(varslayout, query=1, childArray=1)
@@ -146,10 +207,13 @@ class AEaiSeexprTemplate(ShaderAETemplate):
          return
       
       baseNameAttr = nodeAttr
-      baseValueAttr = nodeAttr.replace("vparam_name", "vparam_value");
+      baseValueAttr = nodeAttr.replace("vparam_name", "vparam_value")
+      if baseValueAttr == nodeAttr:
+         baseValueAttr = nodeAttr.replace("vparamName", "vparamValue")
       
       for i in xrange(index+1, len(children)):
-         rembtn, namefld, _, val0fld, val1fld, val2fld = cmds.formLayout(children[i], query=1, childArray=1)
+         #rembtn, namefld, _, val0fld, val1fld, val2fld = cmds.formLayout(children[i], query=1, childArray=1)
+         rembtn, namefld, _, val0fld, val1fld, val2fld = cmds.formLayout(children[i-1], query=1, childArray=1)
          
          indexStr = "[%d]" % (i - 1)
          nextIndexStr = "[%d]" % i
@@ -157,12 +221,45 @@ class AEaiSeexprTemplate(ShaderAETemplate):
          nameAttr = baseNameAttr + indexStr
          valueAttr = baseValueAttr + indexStr
          
-         cmds.setAttr(nameAttr, cmds.getAttr(baseNameAttr + nextIndexStr), type="string")
+         # Disconnect target attribute
+         conns = cmds.listConnections(nameAttr, s=1, d=0, sh=1, p=1)
+         if conns:
+            cmds.disconnectAttr(conns[0], nameAttr)
+         # Set or connect target attribute
+         conns = cmds.listConnections(baseNameAttr + nextIndexStr, s=1, d=0, sh=1, p=1)
+         if conns:
+            cmds.connectAttr(conns[0], nameAttr)
+         else:
+            cmds.setAttr(nameAttr, cmds.getAttr(baseNameAttr + nextIndexStr), type="string")
+         
+         # Disconnect target attribute
+         conns = cmds.listConnections(valueAttr, s=1, d=0, sh=1, p=1)
+         if conns:
+            cmds.disconnectAttr(conns[0], valueAttr)
+         for sub in (".vparamValueX", ".vparamValueY", ".vparamValueZ"):
+            conns = cmds.listConnections(valueAttr + sub, s=1, d=0, sh=1, p=1)
+            if conns:
+               cmds.disconnectAttr(conns[0], valueAttr + sub)
+         # Set or connect target attribute components
          value = cmds.getAttr(baseValueAttr + nextIndexStr)[0]
-         cmds.setAttr(valueAttr, *value);
+         for sub, val in ((".vparamValueX", value[0]), (".vparamValueY", value[1]), (".vparamValueZ", value[2])):
+            conns = cmds.listConnections(baseValueAttr + nextIndexStr + sub, s=1, d=0, sh=1, p=1)
+            if conns:
+               cmds.connectAttr(conns[0], valueAttr + sub)
+            else:
+               cmds.setAttr(valueAttr + sub, val)
+         # Connect target attribute as a whole
+         conns = cmds.listConnections(baseValueAttr + nextIndexStr, s=1, d=0, sh=1, p=1)
+         if conns:
+            cmds.connectAttr(conns[0], valueAttr)
+         #cmds.setAttr(valueAttr, *value);
+         
+         # => must update remove callback
          
          self.setupVariableNameCallback(nameAttr, namefld)
          self.setupVectorVariableValueCallback(valueAttr, val0fld, val1fld, val2fld)
+         self.updateVectorVariableConnectivity(valueAttr, val0fld, val1fld, val2fld)
+         
          cmds.button(rembtn, edit=1, command=lambda *args: self.removeVectorVariable(nodeAttr, varslayout, i-1))
       
       cmds.deleteUI(children[index])
@@ -191,11 +288,21 @@ class AEaiSeexprTemplate(ShaderAETemplate):
       val1fld = cmds.floatField(value=value[1])
       val2fld = cmds.floatField(value=value[2])
       
-      cmds.setAttr(nameAttr, ("vparam%d" % n if name is None else name), type="string")
-      cmds.setAttr(valueAttr, *value)
+      if not cmds.listConnections(nameAttr, s=1, d=0):
+         cmds.setAttr(nameAttr, ("vparam%d" % n if name is None else name), type="string")
+      #cmds.setAttr(valueAttr, *value)
+      if not cmds.listConnections(valueAttr, s=1, d=0):
+         # not fully connected, but could still be connected at component level
+         if not cmds.listConnections(valueAttr+".vparamValueX", s=1, d=0):
+            cmds.setAttr(valueAttr+".vparamValueX", value[0])
+         if not cmds.listConnections(valueAttr+".vparamValueY", s=1, d=0):
+            cmds.setAttr(valueAttr+".vparamValueY", value[1])
+         if not cmds.listConnections(valueAttr+".vparamValueZ", s=1, d=0):
+            cmds.setAttr(valueAttr+".vparamValueZ", value[1])
       
       self.setupVariableNameCallback(nameAttr, namefld)
       self.setupVectorVariableValueCallback(valueAttr, val0fld, val1fld, val2fld)
+      self.updateVectorVariableConnectivity(valueAttr, val0fld, val1fld, val2fld)
       cmds.button(rembtn, edit=1, command=lambda *args: self.removeVectorVariable(nodeAttr, varslayout, n))
       
       cmds.formLayout(form, edit=1,
@@ -240,6 +347,7 @@ class AEaiSeexprTemplate(ShaderAETemplate):
       elemNameAttr = "%s[%d]" % (nameAttr, idx)
       elemValueAttr = "%s[%d]" % (valueAttr, idx)
       
+      topLayout = cmds.formLayout(layout, query=1, parent=1)
       children = cmds.formLayout(layout, query=1, childArray=1)
       
       cmds.textField(children[1], edit=1, text=cmds.getAttr(elemNameAttr))
@@ -247,11 +355,14 @@ class AEaiSeexprTemplate(ShaderAETemplate):
       
       self.setupVariableNameCallback(elemNameAttr, children[1])
       self.setupFloatVariableValueCallback(elemValueAttr, children[3])
+      self.updateFloatVariableConnectivity(elemValueAttr, children[3])
+      cmds.button(children[0], edit=1, command=lambda *args: self.removeFloatVariable(nameAttr, topLayout, idx))
    
    def syncVectorVariable(self, nameAttr, valueAttr, idx, layout):
       elemNameAttr = "%s[%d]" % (nameAttr, idx)
       elemValueAttr = "%s[%d]" % (valueAttr, idx)
 
+      topLayout = cmds.formLayout(layout, query=1, parent=1)
       children = cmds.formLayout(layout, query=1, childArray=1)
       
       value = cmds.getAttr(elemValueAttr)
@@ -264,6 +375,8 @@ class AEaiSeexprTemplate(ShaderAETemplate):
 
       self.setupVariableNameCallback(elemNameAttr, children[1])
       self.setupVectorVariableValueCallback(elemValueAttr, children[3], children[4], children[5])
+      self.updateVectorVariableConnectivity(elemValueAttr, children[3], children[4], children[5])
+      cmds.button(children[0], edit=1, command=lambda *args: self.removeVectorVariable(nameAttr, topLayout, idx))
    
    def syncNameValueArrays(self, nameAttr, valueAttr, vectorValues=False):
       count0 = cmds.getAttr(nameAttr, size=1)
